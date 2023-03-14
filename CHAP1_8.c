2238c9a1ee338cd75cb6e2ee8c270d75e6eaeec2
Caruh <54075633+bornate@users.noreply.github.com>
2023-03-14 20:06:18 +0900

    Create CHAP1_8.c

---
+++ CHAP1_8.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) 
+{
+	//불기연도를 서기연도로 계산해주는 프로그램을 작성해보자
+	int year;
+	scanf("%d", &year);
+	printf("%d", year - 543);
+	return 0;
+}
