3fb65fd0598c97eb295a4371392b884014319932
Caruh <54075633+bornate@users.noreply.github.com>
2023-03-16 00:54:52 +0900

    Create CHAP1_13.c

---
+++ CHAP1_13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char a[] = "|\\_/|";
+	char b[] = "|q p|   /}";
+	char c[] = "( 0 )\"\"\"\\";
+	char d[] = "|\"^\"`    |";
+	char e[] = "||_/=\\\\__|";
+
+	printf("%s\n", a);
+	printf("%s\n", b);
+	printf("%s\n", c);
+	printf("%s\n", d);
+	printf("%s\n", e);
+
+	return 0;
+}
